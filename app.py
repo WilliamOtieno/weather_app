f7e9d30bb6c84e49983ceb08c66285bd4dd893a2
William Otieno <jimmywilliamotieno@gmail.com>
2020-05-23 22:32:45 +0300

    Commit

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import requests
-from flask import Flask, render_template ,request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///weather.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50), nullable = False)
 
-@app.route('/', methods = ['GET', 'POST'])
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'POST':
         new_city = request.form.get('city')
 
         if new_city:
-            new_city_obj = City(name = new_city)
+            new_city_obj = City(name=new_city)
             db.session.add(new_city_obj)
             db.session.commit()
 
@@ -34,13 +36,11 @@ def index():
 
         weather = {
 
-        "city": city.name,
-        "temperature": r['main']['temp'],
-        "description": r['weather'][0]['description'],
-        "icon": r['weather'][0]['icon'],
+            "city": city.name,
+            "temperature": r['main']['temp'],
+            "description": r['weather'][0]['description'],
+            "icon": r['weather'][0]['icon'],
 
         }
         weather_data.append(weather)
-
-
-    return render_template('weather.html', weather_data = weather_data)
+    return render_template('weather.html', weather_data=weather_data)
